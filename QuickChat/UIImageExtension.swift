92e97ef425b001e266407d0466db1557b7932cb1
Hector Castillo <turb0@Hectors-MacBook-Pro.local>
2017-08-10 13:32:06 -0400

    navigation bar ui added

---
+++ QuickChat/UIImageExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageExtension.swift
+//  QuickChat
+//
+//  Created by Hector Castillo on 8/10/17.
+//  Copyright © 2017 Hector Castillo. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    class func imageWithColor(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 2.0)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
--- QuickChat/ViewController.swift
+++ QuickChat/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //set Navigation bar attributes for app wide navigation bar style this would be more easily set in AppDelegate
+        self.title = "CHAT WITH US"
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.black.withAlphaComponent(0.1)]
+        navigationController?.navigationBar.setBackgroundImage(UIImage.imageWithColor(color: UIColor.white), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage.imageWithColor(color: UIColor.black.withAlphaComponent(0.3))
     }
 
     override func didReceiveMemoryWarning() {
