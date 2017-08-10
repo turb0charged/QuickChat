a82b373ff526a7bff47f0cdb0c889627b451a3fc
Hector Castillo <turb0@Hectors-MacBook-Pro.local>
2017-08-10 12:56:24 -0400

    initial setup w/ textfield ui

---
+++ QuickChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickChat
+//
+//  Created by Hector Castillo on 8/10/17.
+//  Copyright © 2017 Hector Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
